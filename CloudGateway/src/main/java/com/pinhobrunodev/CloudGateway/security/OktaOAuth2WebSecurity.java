c0ad640456328a552844c29b4e70be84fec68047
pinhobrunodev <iaishowcs@gmail.com>
2023-06-23 09:16:04 -0300

    Adding Security in API Gateway

--- CloudGateway/src/main/java/com/pinhobrunodev/CloudGateway/security/OktaOAuth2WebSecurity.java
+++ CloudGateway/src/main/java/com/pinhobrunodev/CloudGateway/security/OktaOAuth2WebSecurity.java
@@ -1,2 +1,27 @@
-package com.pinhobrunodev.CloudGateway.security;public class OktaOAuth2WebSecurity {
+package com.pinhobrunodev.CloudGateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+
+    }
 }
